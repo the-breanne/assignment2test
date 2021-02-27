9e548ebf73e7201b2afa873f2b344e6a85d1d760
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-02-27 12:35:05 -0600

    Update settings.py

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -120,12 +120,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
-STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
 
-# Extra places for collectstatic to find static files.
+STATIC_ROOT = os.path.normpath(os.path.join(BASE_DIR, 'staticfiles'))
 STATICFILES_DIRS = (
     os.path.join(BASE_DIR, 'static'),
 )
+
+
+
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
