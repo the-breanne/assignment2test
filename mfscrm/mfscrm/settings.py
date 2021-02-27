3906eadcf500660d10d933792f03cbe51d38d537
the-breanne <77354893+the-breanne@users.noreply.github.com>
2021-02-27 12:29:39 -0600

    trying to fix collectstatic error

--- mfscrm/mfscrm/settings.py
+++ mfscrm/mfscrm/settings.py
@@ -120,6 +120,12 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/3.1/howto/static-files/
 
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 STATIC_URL = '/static/'
+
+# Extra places for collectstatic to find static files.
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, 'static'),
+)
 LOGIN_REDIRECT_URL = '/'
 LOGOUT_REDIRECT_URL = '/'
